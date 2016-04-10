46ed8c5584435fcecc93da449df25cde7f3fbbf6
Daniel <danyjunor96@gmail.com>
2016-04-10 15:53:55 +0300

    Сode reduction

--- JQuery plugins/paralax.js
+++ JQuery plugins/paralax.js
@@ -6,31 +6,25 @@
 
   $.fn.paralax = function( options ) {
 
-   var settings = $.extend( {
+    var settings = $.extend( {
       'speed':'5'
     }, options);
 
-    //this.addClass("paralax-element");
     var $thisElement= this.attr("class");
     var position = $("."+$thisElement).position();
     var positionT = position.top;
     var elementH = this.height();
 
     $(document).ready(function(){
-      var scrollT = $( window ).scrollTop() - positionT;
-      var blockEnd= positionT + elementH;
-      if($( window ).scrollTop() + window.innerHeight >= positionT)
-      {
-        var tp = scrollT*100/blockEnd;
-        $("."+$thisElement).css('background-position', 'center '+(50-(tp/(11-settings.speed)))+'%');
-        $("."+$thisElement).children().each(function(index, element){
-          var weight = $(element).attr("data-weight");
-          $(element).css("margin-top",(tp/(weight))+"%");
-        });
-      }
+      paralaxIt();
     });
 
     $( window ).scroll(function(){
+      paralaxIt();
+    });
+
+    function paralaxIt()
+    {
       var scrollT = $( window ).scrollTop() - positionT;
       var blockEnd= positionT + elementH;
       if($( window ).scrollTop() + window.innerHeight >= positionT)
@@ -43,6 +37,7 @@
         });
         $("."+$thisElement).css("filter","blur("+(tp/(11-settings.speed))+"px)")
       }
-    });
+    }
+
   };
 })( jQuery );
